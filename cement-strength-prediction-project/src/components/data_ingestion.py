7443976adfcb394d35fbb97e09d060ea11fa0307
Shivam Tyagi <shivam1612tyagi@gmail.com>
2023-07-28 12:07:36 +0530

    Update data_ingestion.py

--- cement-strength-prediction-project/src/components/data_ingestion.py
+++ cement-strength-prediction-project/src/components/data_ingestion.py
@@ -28,7 +28,7 @@ class DataIngestion:
 
         try:
             df: pd.DataFrame = export_collection_as_dataframe(
-                db_name="ineuron", collection_name="cement"
+                db_name="mlproject", collection_name="cement"
             )
 
             logging.info("Exported collection as dataframe")
